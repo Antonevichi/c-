72574e0ea77da158ce2486605089b6e885d80ece
Antonevich <v.antonevich@mail.ru>
2022-02-03 12:53:23 +0300

    Example010_metodarray

---
+++ Examples/example010_metodarray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 27, 31, 4, 51, 16, 27, 48 };
+
+int n = array.Length;
+int find = 27;
+
+int index = 0;
+
+while (index < n)
+{
+     if(array[index]==find)
+     {
+         Console.WriteLine(index);
+         break;
+     }
+
+    index++;
+}
