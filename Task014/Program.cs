76d8f0681e864eb85d40e2786f27953799722c86
Antonevich <v.antonevich@mail.ru>
2022-02-07 19:01:04 +0300

    Task014

---
+++ Task014/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 14. Найти третью цифру числа или сообщить, что её нет.
+
+int vvodChisla()
+{
+    int number = 0;
+    Console.WriteLine("Введите число:");
+    if (!int.TryParse(Console.ReadLine(), out number))
+    {
+       Console.WriteLine("Введите целое число! По умолчанию установлено значение 0.");
+    }  return number;
+}
+       int a = vvodChisla();
+       if(a > 99)
+    {
+       string b = Convert.ToString(a);
+       Console.WriteLine($"Третья цифра числа: {b[2]}");
+       return;
+    }
+       if(a < -99)
+{
+       string b = Convert.ToString(a);
+       Console.WriteLine($"Третья цифра числа: {b[3]}");
+       return;
+}
+       Console.WriteLine("Третья цифра отсуствует");
