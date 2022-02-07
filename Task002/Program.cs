be05e77a4eb7bc552373b49ddffc77b84c22f710
Antonevich <v.antonevich@mail.ru>
2022-02-07 18:58:42 +0300

    Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Даны два числа. Показать большее и меньшее число
+
+
+using System;
+using System.Linq;
+ 
+class  Program
+{
+    
+    public static void Main()
+    {
+        
+        Console.WriteLine("Введите два числа : ");
+        {
+            Console.WriteLine(String.Join(" большее число, меньшее число ", Console.ReadLine().Split().Select(Int32.Parse).OrderByDescending(x => x)));
+        } 
+       
+    }
+
+       
+}
