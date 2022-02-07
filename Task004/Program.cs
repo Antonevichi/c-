16b3503e7b0f00b0117a9cea87c4c66a7b34d9a5
Antonevich <v.antonevich@mail.ru>
2022-02-07 18:59:16 +0300

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 4: Найти максимальное из трёх чисел.
+
+int a = 43;
+int b = 6;
+int c =26;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+
+Console.Write("максимальное число  = ");
+
+
+Console.WriteLine(max);
