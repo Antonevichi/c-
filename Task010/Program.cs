6493ddbfa6c836fd409b78c34db254a648fdc2be
Antonevich <v.antonevich@mail.ru>
2022-02-07 19:00:20 +0300

    Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10:** Показать вторую цифру трёхзначного числа.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var n = Math.Abs(Convert.ToInt64(Console.ReadLine()));
+
+        while (n >= 100)
+        {
+            n /= 10;
+        }
+
+        var r = n % 10;
+
+        Console.WriteLine(r);
+    }
+}
