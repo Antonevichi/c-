ee2b81d2bf7d3bfc67a4e5eea9430a86558695b7
Antonevich <v.antonevich@mail.ru>
2022-02-07 19:00:39 +0300

    Task012

---
+++ Task012/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 12:** Удалить вторую цифру трёхзначного числа.int a = 123;
+
+int vvodChisla()
+{
+    int number = 0;
+    Console.WriteLine("Введите трехзначное число:");
+    if (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Введите целое число! По умолчанию установлено значение 0.");
+    }return number;
+}
+int a = vvodChisla();
+if (a > 99)
+{
+    if(a < 1000)
+    {
+        int b = a % 10;
+        a = a / 100 * 10 + b;
+        Console.WriteLine($"После удаления  второй цифры получится: {a}");
+        return;
+    }
+}
+if (a < -99)
+    {
+    if(a > -1000)
+    {
+        int b = a % 10;
+        a = a / 100 * 10 + b;
+        Console.WriteLine($"Если удалить вторую цифру получится: {a}");
+        return;
+    }
+}
+Console.WriteLine("Введите трехзначное число!");
