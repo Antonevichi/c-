282a532064972f569eb0e46f1a74ffddc7b26c16
Antonevich <v.antonevich@mail.ru>
2022-02-27 13:20:15 +0300

    Task024

---
+++ Task024/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача № 24. Найти кубы чисел от 1 до N
+
+int a = 0;
+
+System.Console.WriteLine("Введите число ");
+string s = string.Empty;
+
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+for (int i = 1; i <= a; i++)
+{
+    System.Console.Write(i*i*i + " ");
+}
