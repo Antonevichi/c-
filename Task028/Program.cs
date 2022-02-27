f1ce80c4590440baff59082507942c1a24cd9361
Antonevich <v.antonevich@mail.ru>
2022-02-27 13:21:17 +0300

    Task028

---
+++ Task028/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача №28. Подсчитать сумму цифр в числе
+
+int a = 0, sum = 0;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+while (a!= 0)
+{
+    sum = sum + a%10;
+    a = a/10;
+}
+
+System.Console.WriteLine("Сумма цифр в числе равна " + sum);
