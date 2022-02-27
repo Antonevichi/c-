0f975cf7259aa49fcc37e2c80231137cd1fbfee9
Antonevich <v.antonevich@mail.ru>
2022-02-27 13:21:44 +0300

    Task032

---
+++ Task032/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача №32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int [] Arr = new int [8];
+
+for (int i=0;i<Arr.Length;i++)
+    Arr[i]=new Random().Next(0,2);
+
+for (int i=0;i<Arr.Length;i++)
+    System.Console.Write($"{Arr[i] }");
