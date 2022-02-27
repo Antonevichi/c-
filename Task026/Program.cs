899c793b888fa11b3a9ac0d200ed4fef42af8ea8
Antonevich <v.antonevich@mail.ru>
2022-02-27 13:21:03 +0300

    Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача №26. Возведите число А в натуральную степень B используя цикл
+
+int a = 0, b = 0, result = -1;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите число ");
+
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+System.Console.WriteLine("Введите степень числа ");
+
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+
+int Grade(int s, int x)
+{
+    int res = -1;
+    for (int i = 0; i < x; i++)
+    {
+        res = s*s;
+    }
+    return res;
+}
+
+result = Grade(a,b);
+System.Console.WriteLine($"Результат возведения числа " + a + " в степень " + b + " равно " + result);
