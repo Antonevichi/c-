26976f217f2ac170a326f9d7121b38871b2e7241
Antonevich <v.antonevich@mail.ru>
2022-02-27 13:22:00 +0300

    Task034

---
+++ Task034/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача №34. Написать программу замену элементов массива на противоположные
+
+
+int N = 10;
+int [] a = new int [N];
+
+void Input(int [] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(1,100);
+    }
+}
+
+void Solve(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+void Print(int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+}
+Input(a);
+Print(a);
+Solve(a);
+System.Console.WriteLine();
+Print(a);
