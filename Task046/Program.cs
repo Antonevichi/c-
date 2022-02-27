88dae932737c7c9c30dca63a27f0c8230d0ce7a5
Antonevich <v.antonevich@mail.ru>
2022-02-27 13:23:29 +0300

    Task046

---
+++ Task046/Program.cs
@@ -0,0 +1,39 @@
+﻿//задача 46. Написать программу масштабирования фигуры
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Explorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "4";
+            double k = double.Parse(a);
+
+
+            string input = "(0,0) (2,0) (2,2) (0,2)";
+            string[] arrInput = input.Split(' ');
+
+            List<double> numbers = new List<double> { };
+            foreach (string i in arrInput)
+            {
+                string element = i;
+                element = element.Replace("(", "");
+                element = element.Replace(")", "");
+                numbers.Add(double.Parse(element));
+            }
+
+
+            foreach (double i in numbers)
+                Console.Write($"({i * k:F1}) ");
+
+            Console.WriteLine();
+
+        }
+    }
+}
